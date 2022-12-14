10ca0a8059c061913c54f1ac92af71bf290bb79f
Thomas Saigre <saigre@math.unistra.fr>
2022-12-14 16:29:15 +0100

    multiple gathers (to test) #3

--- test/test_MPI/mpi_gather.cpp
+++ test/test_MPI/mpi_gather.cpp
@@ -14,22 +14,41 @@ int gather_array()
     MPI_Comm_rank(MPI_COMM_WORLD , &world_rank );
 
     int master_rank = 0;
-    if (world_rank == master_rank) cout << "Gather vectors" << endl;
     
-    int valsSend[5] = {1, 2, 3, 4, 5};
+    int tab0[4] = {1 + 4*world_rank, 2 + 4*world_rank, 3 + 4*world_rank, 4 + 4*world_rank};
+    int tab1[4] = {11 + 4*world_rank, 12 + 4*world_rank, 13 + 4*world_rank, 14 + 4*world_rank};
 
-    int *valsRec;
+    cout << world_rank << "tab0: ";
+    for (size_t i = 0; i < 4; i++)
+        cout << tab0[i] << " ";
+        cout << endl;
+    cout << world_rank << "tab1: ";
+    for (size_t i = 0; i < 4; i++)
+        cout << tab1[i] << " ";
+    cout << endl;
+
+    int *tab0Gather, *tab1Gather;
     if (world_rank == master_rank)
-        valsRec = new int[world_size * 5];
+    {
+        tab0Gather = new int[world_size * 4];
+        tab1Gather = new int[world_size * 4];
+    }
 
-    MPI_Gather(valsSend, 5, MPI_INT, valsRec, 5, MPI_INT, master_rank, MPI_COMM_WORLD);
+    MPI_Gather(tab0, 4, MPI_INT, tab0Gather, 4, MPI_INT, master_rank, MPI_COMM_WORLD);
+    MPI_Gather(tab1, 4, MPI_INT, tab1Gather, 4, MPI_INT, master_rank, MPI_COMM_WORLD);
 
     if (world_rank == master_rank){
-        size_t size = world_size * 5;
+        size_t size = world_size * 4;
+        cout << "Gather0: ";
         for (size_t i = 0; i < size; i++)
-            cout << valsRec[i] << " ";
+            cout << tab0Gather[i] << " ";
         cout << endl;
-        delete valsRec;
+        cout << "Gather1: ";
+        for (size_t i = 0; i < size; i++)
+            cout << tab1Gather[i] << " ";
+        cout << endl;
+        delete tab0Gather;
+        delete tab1Gather;
     }
     MPI_Finalize();
     return 0;
