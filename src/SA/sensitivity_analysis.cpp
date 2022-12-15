f231d5c0261c7f9766d641ffbea32486a3960983
Thomas Saigre <saigre@math.unistra.fr>
2022-12-15 11:22:17 +0100

    first implementation of parallelization of output computation #3

--- src/SA/sensitivity_analysis.cpp
+++ src/SA/sensitivity_analysis.cpp
@@ -115,7 +115,7 @@ OT::ComposedDistribution composedFromModel(parameter_space_ptr_t Dmu )
         {
             // dist = OT::LogNormal(2., 0.6137056388801094, 8.);
             double s = 0.4; double mu = log(10) - 0.5*s*s;
-            dist = OT::TruncatedDistribution(OT::LogNormal(2.222585092994046, s, 0), OT::Interval(8, 20));
+            dist = OT::TruncatedDistribution(OT::LogNormal(2.222585092994046, s, 0), OT::Interval(8, 100));
         }
         else if (names[d] == "E")
             dist = OT::LogUniform( log(mumin(d)), log(mumax(d)) );
@@ -138,6 +138,7 @@ OT::Sample output(OT::Sample const& input, plugin_ptr_t const& plugin, Eigen::Ve
 {
     size_t n = input.getSize();
     OT::Sample output(n, 1);
+    output.setDescription( {Feel::soption(Feel::_name="output.name")} );
     {
         parameter_space_ptr_t Dmu = plugin->parameterSpace();
         std::vector<std::string> names = Dmu->parameterNames();
@@ -187,6 +188,10 @@ void runSensitivityAnalysis( std::vector<plugin_ptr_t> plugin, size_t sampling_s
 {
     using namespace Feel;
 
+    int world_size = Feel::Environment::worldComm().globalSize(),
+        world_rank = Feel::Environment::worldComm().globalRank(),
+        master_rank = Feel::Environment::worldComm().masterRank();
+
     Feel::cout << "Running sensisivity analysis with a sample of size " << sampling_size << std::endl;
 
     bool loadFiniteElementDatabase = boption(_name="crb.load-elements-database");
@@ -201,7 +206,15 @@ void runSensitivityAnalysis( std::vector<plugin_ptr_t> plugin, size_t sampling_s
     std::vector<std::string> tableRowHeader = muspace->parameterNames();
     size_t dim = muspace->dimension();
 
+    size_t input_sample_gather_size = sampling_size * dim * world_size,
+           input_sample_local_size = sampling_size * dim;
+    size_t output_sample_gather_size = sampling_size * world_size,
+           output_sample_local_size = sampling_size;
+
     double adapt_tol = doption(_name="adapt.tol");
+
+    double *input_sample_gather, *output_sample_gather;
+    OT::Sample input_sample, output_sample;
 
 
     if ( !boption("algo.poly") )
@@ -247,28 +260,67 @@ void runSensitivityAnalysis( std::vector<plugin_ptr_t> plugin, size_t sampling_s
             for (int r=0; r<nrun; ++r)
             {
                 Feel::cout << tc::bold << tc::red << "Run " << r+1 << " over " << nrun << " with sample of size " << sampling_size << tc::reset << std::endl;
-                OT::Sample input_sample = composed_distribution.getSample(sampling_size);
-                OT::Sample output_sample = output(input_sample, plugin[0], time_crb, online_tol, rbDim);
-                OT::FunctionalChaosAlgorithm polynomialChaosAlgorithm = OT::FunctionalChaosAlgorithm(input_sample, output_sample);
+                OT::Sample input_sample_proc = composed_distribution.getSample(sampling_size);
+                OT::Sample output_sample_proc = output(input_sample_proc, plugin[0], time_crb, online_tol, rbDim);
 
-                polynomialChaosAlgorithm.run();
-                OT::FunctionalChaosResult polynomialChaosResult = polynomialChaosAlgorithm.getResult();
-                OT::FunctionalChaosSobolIndices sensitivityAnalysis = OT::FunctionalChaosSobolIndices(polynomialChaosResult);
-                for (size_t i=0; i<dim; ++i)
+                std::cout << "On proc " << world_rank << " : input_sample_proc = \n" << input_sample_proc << std::endl;
+                std::cout << "On proc " << world_rank << " : output_sample_proc = \n" << output_sample_proc << std::endl;
+
+                if ( Feel::Environment::worldComm().isMasterRank() )
                 {
-                    indices(r, i) = sensitivityAnalysis.getSobolIndex(i);
-                    o1 = sensitivityAnalysis.getSobolIndex(i);
-                    ot = sensitivityAnalysis.getSobolTotalIndex(i);
-                    if ( o1 > ot )
-                    {
-                        Feel::cout << tc::red << "Warning: o1 > ot" << tc::reset << std::endl;
-                        throw std::logic_error("Issue in computing sobol indices");
-                    }
-                    res.setIndice( o1, i, 1 );
-                    res.setIndice( ot, i, 0 );
+                    input_sample_gather = new double[input_sample_gather_size];
+                    output_sample_gather = new double[output_sample_gather_size];
+                    input_sample = OT::Sample( output_sample_gather_size, dim );
+                    input_sample.setDescription( input_sample_proc.getDescription() );
+                    output_sample = OT::Sample( output_sample_gather_size, 1 ); 
+                    output_sample.setDescription( {soption(_name="output.name")} );                   
                 }
+
+                // Gather input_sample : the parameters used for the computation of the output
+                MPI_Gather( input_sample_proc.data(), input_sample_local_size, MPI_DOUBLE,
+                            input_sample_gather, input_sample_local_size, MPI_DOUBLE,
+                            master_rank, Feel::Environment::worldComm().globalComm() );
+
+                // Gather output_sample : the output computed with the parameters
+                MPI_Gather( output_sample_proc.data(), output_sample_local_size, MPI_DOUBLE,
+                            output_sample_gather, output_sample_local_size, MPI_DOUBLE,
+                            master_rank, Feel::Environment::worldComm().globalComm() );
+
+                if ( Feel::Environment::worldComm().isMasterRank() )
+                {
+
+                    for (size_t i=0; i<output_sample_gather_size; ++i)
+                    {
+                        for (size_t j=0; j<dim; ++j)
+                            input_sample[i][j] = input_sample_gather[i*dim + j];
+                        output_sample[i][0] = output_sample_gather[i];
+                    }
+
+                    Feel::cout << "After gathering" << std::endl;
+                    Feel::cout << input_sample << std::endl;
+                    Feel::cout << output_sample << std::endl;
+
+                    OT::FunctionalChaosAlgorithm polynomialChaosAlgorithm = OT::FunctionalChaosAlgorithm(input_sample, output_sample);
+                    polynomialChaosAlgorithm.run();
+                    OT::FunctionalChaosResult polynomialChaosResult = polynomialChaosAlgorithm.getResult();
+                    OT::FunctionalChaosSobolIndices sensitivityAnalysis = OT::FunctionalChaosSobolIndices(polynomialChaosResult);
+                    for (size_t i=0; i<dim; ++i)
+                    {
+                        indices(r, i) = sensitivityAnalysis.getSobolIndex(i);
+                        o1 = sensitivityAnalysis.getSobolIndex(i);
+                        ot = sensitivityAnalysis.getSobolTotalIndex(i);
+                        if ( o1 > ot )
+                        {
+                            Feel::cout << tc::red << "Warning: o1 > ot" << tc::reset << std::endl;
+                            throw std::logic_error("Issue in computing sobol indices");
+                        }
+                        res.setIndice( o1, i, 1 );
+                        res.setIndice( ot, i, 0 );
+                    }
+                }
+
             }
-            std::cout << "indices = \n" << indices << std::endl;
+            Feel::cout << "indices = \n" << indices << std::endl;
 
             OT::Scalar std_max = indices.computeStandardDeviation().normInf();
             Feel::cout << tc::green << tc::bold << "max diff = " << std_max << " (tol=" << adapt_tol << ")" << tc::reset << std::endl;
@@ -282,6 +334,7 @@ void runSensitivityAnalysis( std::vector<plugin_ptr_t> plugin, size_t sampling_s
                 sampling_size *= 2;
                 res.setSamplingSize( sampling_size );
             }
+            stop = true;
         }
 
         res.print();
@@ -310,6 +363,7 @@ int main( int argc, char** argv )
         ( "sampling.type", po::value<std::string>()->default_value( "random" ), "type of sampling" )
         ( "rb-dim", po::value<int>()->default_value( -1 ), "reduced basis dimension used (-1 use the max dim)" )
         ( "output_results.save.path", po::value<std::string>(), "output_results.save.path" )
+        ( "output.name" , po::value<std::string>()->default_value("s_N"), "name of the output" )
 
         ( "algo.poly", po::value<bool>()->default_value(true), "use polynomial chaos" )
         ( "algo.nrun", po::value<int>()->default_value(5), "number to run algorithm" )
@@ -336,7 +390,7 @@ int main( int argc, char** argv )
                      _desc_lib = crbonlinerunliboptions.add( feel_options() ),
                      _about = makeAbout() );
 
-    OT::RandomGenerator::SetSeed( ::time(NULL) );
+    OT::RandomGenerator::SetSeed( ::time(NULL) * Feel::Environment::worldComm().globalRank() );
     plugin_ptr_t plugin = loadPlugin();
     // runCrbOnline( { plugin } );
     runSensitivityAnalysis( { plugin }, ioption(_name="sampling.size"), false );
